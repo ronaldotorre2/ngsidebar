9785535fd0d44f704577786d940062f95615887e
Ronaldo Torre <ronaldo.torre@outlook.com>
2019-01-26 16:16:45 -0200

    Import project Angular Sidebar Component

---
+++ src/app/shared/constant.ts
@@ -0,0 +1,6 @@
+export class Constant {
+  public static readonly APP_INITIAL = "MYAPP";
+  public static readonly APPNAME = "Menu Aplicativo";
+  public static readonly COMPANY = "Minha Empresa";
+  public static readonly COMPANY_INITIAL = "ETIT";
+}
